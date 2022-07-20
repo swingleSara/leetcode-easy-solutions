5e459c68825951fd3fd471defa3b8e47c4537f4a
Sara Swingle <sara.swingle.dev@gmail.com>
2022-07-19 22:26:12 -0600

    Add easy solutions

---
+++ 1TwoSum.js
@@ -0,0 +1,9 @@
+const twoSum = (nums, target) => {
+  for (let i = 0; i < nums.length - 1; i++) {
+    for (let j = 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target && i !== j) {
+        return [i, j];
+      }
+    }
+  }
+};
